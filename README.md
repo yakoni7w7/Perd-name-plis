b36d4415a02250fbacb9cde84e3bd580bd9b1109
yakoni7w7 <josesoriatorrico@gmail.com>
2025-10-05 17:04:35 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Perd-name-plis
+# Perdoname
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Render](https://img.shields.io/badge/Render-%46E3B7.svg?style=for-the-badge&logo=render&logoColor=white)
+
+❤ Proyecto para los bros que necesitan el perdón 💔...
+
+La idea original fue tomada del [siguiente repo](https://github.com/namdosanwannabe/be-my-valentine)
+
+Si te gustó deja tu estrellita 🌟
+
+## 🖼 Capturas de pantalla
+
+![Inicio](/assets/1.png)
+
+![Medio](/assets/3.png)
+
+![Fin](/assets/2.png)
+
+## 🚀 Despliegue en Render
+
+Para acceder a la página, ingrese al [siguiente enlace](https://perdoname.onrender.com)
---
+++ script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 10;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    resizeNoButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  } else if (noCount === 13) noButton.style.display = "none";
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Siiiiii!!!! 😍😍😍";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+  window.navigator.vibrate(2000);
+  catImg.addEventListener(
+    "dblclick",
+    () => (titleElement.innerHTML = "Hecho con amor por EduardoProfe666🎩")
+  );
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.3;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function resizeNoButton() {
+  const computedStyle = window.getComputedStyle(noButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+
+  noButton.style.fontSize = `${fontSize * 0.9}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "Estás segura?",
+    "Por favorcito",
+    "Perdóname chica anda",
+    "No seas mala",
+    "Yo no lo vuelvo a hacer...",
+    "Discúlpame pls",
+    "Anda mi amor perdóname",
+    "No me hagas esto :(",
+    "Me rompes el corazón",
+    "Quiero llorar...",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
